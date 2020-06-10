bc32293774941b094ac0c883340ee45d7d712c76
Samantha Wabi <swfullpotential@gmail.com>
2020-06-10 03:28:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,4 +44,17 @@ class Owner
    self.cats.each {|cat| cat.mood = "happy"}
  end
  
+ def sell_pets
+   self.cats.each do |cat|
+     self.cats.delete(cat)
+     cat.owner = nil 
+     cat.mood = "nervous"
+   end
+   self.dogs.each do |dog|
+     self.dogs.delete(dog)
+     dog.owner = nil 
+     dog.mood = "nervous"
+   end
+ end
+ 
 end
